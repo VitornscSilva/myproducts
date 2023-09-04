056a56dac956bd0fb4fbc74183e7edd902e5aa18
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-09-04 02:19:51 -0300

    feat: add swagger

--- api-gateway/src/app/index.ts
+++ api-gateway/src/app/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from '../swagger.json';
 
 import cors from './middlewares/cors'
 import routes from '../routes/index.routes';
@@ -8,5 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors);
 app.use('/api', routes);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 export default app;
